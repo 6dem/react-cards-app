ce7e549b18c3e362533d3b25ba920067a06b06b5
Edem <edem.ablaev57@yandex.ru>
2025-05-06 15:12:12 +0300

    add NotfoundPage component and QuestionCard dynamic mod

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { MainLayout } from "./components/MainLayout"
 import { HomePage } from "./pages/HomePage"
+import { NotFoundPage } from "./pages/NotFoundPage"
 
 function App() {
     return (
@@ -8,9 +9,11 @@ function App() {
             <Routes>
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/main" element={<div>Main component 😍</div>} />
                     <Route path="/forbidden" element={<div>NO WAY</div>} />
                     <Route path="/add-question" element={<div>Add question</div>} />
+                    <Route path="/question/:id" element={<div>Question page</div>} />
+
+                    <Route path="*" element={<NotFoundPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/components/QuestionCard/QuestionCard.jsx
+++ src/components/QuestionCard/QuestionCard.jsx
@@ -1,24 +1,25 @@
+import { useNavigate } from "react-router-dom"
 import { Button } from "../Button"
 import cls from "./QuestionCard.module.css"
 
-export const QuestionCard = () => {
+export const QuestionCard = (props) => {
+    const navigate = useNavigate()
+
     return (
         <div className={cls.card}>
             <div className={cls.cardLabels}>
-                <div>Level: 1</div>
-                <div>Not Comleted</div>
+                <div>Level: {props.card.level}</div>
+                <div>{props.card.completed ? "Completed" : "Not Comleted"}</div>
             </div>
 
-            <h5 className={cls.cardTitle}>Что такое JSX?</h5>
+            <h5 className={cls.cardTitle}>{props.card.question}</h5>
 
             <div className={cls.cardAnswerWrapper}>
                 <label>short answer: </label>
-                <p className={cls.cardAnswer}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, voluptatem?
-                </p>
+                <p className={cls.cardAnswer}>{props.card.answer}</p>
             </div>
 
-            <Button onClick={() => {}}>View</Button>
+            <Button onClick={() => navigate(`/question/${props.card.id}`)}>View</Button>
         </div>
     )
 }
